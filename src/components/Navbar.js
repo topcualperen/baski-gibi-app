23a9e2f1c068af2d4cc6fe9d7e22f783c8b3acf8
Alperen Topcu <alperenttpc78@gmail.com>
2022-10-27 18:53:09 +0300

    main page

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from "react"
+import "./Navbar.css"
+
+const Navbar = () => {
+     return (
+          <nav className="nav-class">
+               <div className="logo">
+               <a href="http://localhost:3000/">
+                        <h5>
+                            Baskı Gibi     
+                        </h5>
+                    </a>
+               </div>
+               
+          </nav>
+     )
+}
+
+export default Navbar;
